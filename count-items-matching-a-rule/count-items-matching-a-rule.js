2440f22cd115932843a0a7b977f6650b2e06a1cb
gabrielpasquantonio <64622616+gabrielpasquantonio@users.noreply.github.com>
2021-03-08 22:32:01 -0300

    Time: 100 ms (100.00%), Space: 43 MB (100.00%) - LeetHub

---
+++ count-items-matching-a-rule/count-items-matching-a-rule.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string[][]} items
+ * @param {string} ruleKey
+ * @param {string} ruleValue
+ * @return {number}
+ */
+var countMatches = function(items, ruleKey, ruleValue) {
+    let count = 0;
+    if (ruleKey === 'type'){
+        for(let i = 0; i < items.length; i++){
+            if(items[i][0] === ruleValue){
+                count ++
+            }
+            }
+    }
+     if (ruleKey === 'color'){
+        for(let i =0; i < items.length; i++){
+            if(items[i][1] === ruleValue){
+                count ++
+            }
+            
+        }
+    }
+   if (ruleKey === 'name'){
+        for(let i =0; i < items.length; i++){
+            if(items[i][2] === ruleValue){
+                count ++
+            }
+           } 
+        }
+       return count
+};
