b072885e0aa5178daf24b1ce59a49e5a03de2ebb
gabrielpasquantonio <64622616+gabrielpasquantonio@users.noreply.github.com>
2021-04-22 12:58:59 -0300

    Time: 80 ms (57.88%), Space: 39 MB (83.01%) - LeetHub

---
+++ increasing-order-search-tree/increasing-order-search-tree.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var increasingBST = function(root) {
+    let head = new TreeNode('dummy');
+    let cursor = head;
+    
+    
+    inorder(root)
+    return head.right; 
+    function inorder(node){
+        if(!node) return;
+        
+        inorder(node.left);
+        
+        cursor.right = new TreeNode(node.val)
+        cursor = cursor.right;
+        
+        inorder(node.right)
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+};
