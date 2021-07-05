139f811f56ccd6124bb44977f67e1f235f710a6a
gabrielpasquantonio <64622616+gabrielpasquantonio@users.noreply.github.com>
2021-07-05 17:22:59 -0300

    Time: 60 ms (99.86%), Space: 39.9 MB (28.30%) - LeetHub

---
+++ valid-parentheses/valid-parentheses.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    
+
+
+let bracket = {
+    '(':')',
+    '[':']',
+    '{':'}'
+    
+}
+    
+    let heap = [];
+    for(let char of s){
+        if(bracket[char]){
+            heap.push(bracket[char])
+        }else{
+            if(heap.pop() !== char) return false
+        }
+    }
+return (!heap.length)
+};
